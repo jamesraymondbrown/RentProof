c72f1bcb28ed294a9ee4c8cba3aa87f359162852
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-02 15:49:24 -0800

    begin integrating maps api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 function App() {
+
+  const { isLoaded } = useLoadScript({googleMapsApiKey: "process.env.GOOGLE_API_KEY"})
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Map
         </p>
+        <GoogleMap zoom={10} center={{}} mapContainerClassName="map-container"></GoogleMap>
         <a
           className="App-link"
           href="https://reactjs.org"
