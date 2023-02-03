76ae1bd7e595412734b31fa72b2a516e2e4b5f84
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-03 13:06:58 -0800

    add a pin to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,11 @@ export default function Map(props) {
   const { isLoaded } = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY})
 
   function Map() {
-    return <GoogleMap zoom={13} center={{lat: 49.28, lng: -123.12}} mapContainerClassName="map-container"></GoogleMap>
+    return (
+    <GoogleMap zoom={13} center={{lat: 49.28, lng: -123.12}} mapContainerClassName="map-container">
+      <Marker position={{ lat: 49.2608632, lng: -123.1189717}} />
+    </GoogleMap>
+    );
   }
 
 
