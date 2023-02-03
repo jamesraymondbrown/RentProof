a86a323e34570beb4b79499357931a226a03cd3d
Hadley <sutherlandhadley@gmail.com>
2023-02-03 13:01:57 -0700

    add rentlist to the side of the homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.jsx';
+import RentList from "./components/RentList";
 
 function App() {
 
   return (
-    <div className="App">
-      <Map />
+    <div className="App" style={{ display: "flex" }}>
+      <Map style={{ flex: 3 }} />
+      <RentList style={{ flex: 1, width: "25%" }} />
     </div>
   );
 }
