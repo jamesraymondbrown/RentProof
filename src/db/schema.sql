9ad62a72bd855af0d02c93325a44d64a0ca991c3
Michael Green <greenmr95@hotmail.com>
2023-02-03 16:48:13 -0800

    update schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,43 +1,32 @@
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS prices CASCADE;
-DROP TABLE IF EXISTS submissions CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  square_feet VARCHAR(255) NOT NULL,
-  number_of_bedrooms INTEGER NOT NULL,
-  number_of_bathrooms INTEGER NOT NULL,
+  province VARCHAR(255),
   city VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  picture VARCHAR(255) NOT NULL
+  street_address VARCHAR(255) NOT NULL,
+  postcode VARCHAR(255)
 );
 
 CREATE TABLE prices (
   id SERIAL PRIMARY KEY NOT NULL,
   property_id INTEGER REFERENCES properties(id),
-  date DATE NOT NULL,
-  price NUMERIC,
-  documentation VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE submissions (
-  id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
+  admin_status VARCHAR(255) NOT NULL DEFAULT "pending",
+  date DATE NOT NULL,
+  price NUMERIC NOT NULL,
+  documentation VARCHAR(255) NOT NULL, --url
+  photo VARCHAR(255) NOT NULL, --url 
   square_footage INTEGER NOT NULL,
   number_of_bedrooms INTEGER NOT NULL,
-  number_of_bathrooms INTEGER NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  picture VARCHAR(255) NOT NULL,
-  price NUMERIC,
-  documentation VARCHAR(255) NOT NULL
+  number_of_bathrooms INTEGER NOT NULL,  
 );
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  name email VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 )
-
-
