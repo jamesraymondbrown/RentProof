ef9e50527b1240914d990960ba55115cf72647b2
Hadley <sutherlandhadley@gmail.com>
2023-02-04 23:19:27 -0700

    clean up the map.jsx by moving logic to marker.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import "./Map.scss";
-import axios from "axios";
 import Marker from "./Marker.jsx";
 
 export default function MapDisplay(props) {
@@ -9,18 +8,7 @@ export default function MapDisplay(props) {
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   });
 
-  const [selectedMarker, setSelectedMarker] = useState(null);
 
-  function handleMarkerClick(marker) {
-    setSelectedMarker(marker);
-
-    // GET request using the fetch method
-    const address = `${marker.lat},${marker.lng}`;
-    axios.get(`/api/property/${address}`)
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-  }
 
   function Map() {
     return (
@@ -35,7 +23,7 @@ export default function MapDisplay(props) {
           position={{ lat: 49.28, lng: -123.12 }}
           title={"Example Property 1"}
           label={"1"}
-          onClick={() => handleMarkerClick({ lat: 49.28, lng: -123.12 })}
+          
         />
         <Marker
           bedrooms={2}
@@ -43,7 +31,7 @@ export default function MapDisplay(props) {
           position={{ lat: 49.29, lng: -123.13 }}
           title={"Example Property 2"}
           label={"2"}
-          onClick={() => handleMarkerClick({ lat: 49.29, lng: -123.13 })}
+          
         />
         <Marker
           bedrooms={2}
@@ -51,7 +39,7 @@ export default function MapDisplay(props) {
           position={{ lat: 49.27, lng: -123.14 }}
           title={"Example Property 2"}
           label={"2"}
-          onClick={() => handleMarkerClick({ lat: 49.29, lng: -123.13 })}
+          
         />
       </GoogleMap>
     );
