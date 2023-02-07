281a171d0f2039072d456468c493d9b9aa81cd0d
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-06 20:41:26 -0800

    formatted sql insert query for prices

--- server/helpers/pricesToSQL.js
+++ server/helpers/pricesToSQL.js
@@ -1,88 +1,181 @@
 const studios = [
   [
-    { year: 2014, price: 1080, bedrooms: 0 },
-    { year: 2015, price: 1230, bedrooms: 0 },
-    { year: 2016, price: 1340, bedrooms: 0 },
-    { year: 2017, price: 1350, bedrooms: 0 },
-    { year: 2018, price: 1520, bedrooms: 0 },
-    { year: 2019, price: 1570, bedrooms: 0 },
-    { year: 2020, price: 1590, bedrooms: 0 },
-    { year: 2021, price: 1750, bedrooms: 0 },
-    { year: 2022, price: 1980, bedrooms: 0 },
-    { year: 2023, price: 2190, bedrooms: 0 }
+    {
+      year: 2014,
+      price: 760,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2015,
+      price: 770,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2016,
+      price: 770,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2017,
+      price: 780,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2018,
+      price: 800,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2019,
+      price: 820,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2020,
+      price: 930,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2021,
+      price: 1060,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2022,
+      price: 1110,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    },
+    {
+      year: 2023,
+      price: 1230,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 439
+    }
   ],
   [
-    { year: 2014, price: 840, bedrooms: 0 },
-    { year: 2015, price: 910, bedrooms: 0 },
-    { year: 2016, price: 970, bedrooms: 0 },
-    { year: 2017, price: 1020, bedrooms: 0 },
-    { year: 2018, price: 1160, bedrooms: 0 },
-    { year: 2019, price: 1250, bedrooms: 0 },
-    { year: 2020, price: 1320, bedrooms: 0 },
-    { year: 2021, price: 1440, bedrooms: 0 },
-    { year: 2022, price: 1480, bedrooms: 0 },
-    { year: 2023, price: 1670, bedrooms: 0 }
-  ],
-  [
-    { year: 2014, price: 1040, bedrooms: 0 },
-    { year: 2015, price: 1100, bedrooms: 0 },
-    { year: 2016, price: 1200, bedrooms: 0 },
-    { year: 2017, price: 1250, bedrooms: 0 },
-    { year: 2018, price: 1310, bedrooms: 0 },
-    { year: 2019, price: 1460, bedrooms: 0 },
-    { year: 2020, price: 1650, bedrooms: 0 },
-    { year: 2021, price: 1760, bedrooms: 0 },
-    { year: 2022, price: 2010, bedrooms: 0 },
-    { year: 2023, price: 2200, bedrooms: 0 }
-  ],
-  [
-    { year: 2014, price: 1010, bedrooms: 0 },
-    { year: 2015, price: 1050, bedrooms: 0 },
-    { year: 2016, price: 1100, bedrooms: 0 },
-    { year: 2017, price: 1190, bedrooms: 0 },
-    { year: 2018, price: 1320, bedrooms: 0 },
-    { year: 2019, price: 1390, bedrooms: 0 },
-    { year: 2020, price: 1400, bedrooms: 0 },
-    { year: 2021, price: 1530, bedrooms: 0 },
-    { year: 2022, price: 1610, bedrooms: 0 },
-    { year: 2023, price: 1660, bedrooms: 0 }
-  ],
-  [
-    { year: 2014, price: 760, bedrooms: 0 },
-    { year: 2015, price: 790, bedrooms: 0 },
-    { year: 2016, price: 870, bedrooms: 0 },
-    { year: 2017, price: 910, bedrooms: 0 },
-    { year: 2018, price: 1010, bedrooms: 0 },
-    { year: 2019, price: 1030, bedrooms: 0 },
-    { year: 2020, price: 1090, bedrooms: 0 },
-    { year: 2021, price: 1110, bedrooms: 0 },
-    { year: 2022, price: 1220, bedrooms: 0 },
-    { year: 2023, price: 1250, bedrooms: 0 }
-  ],
-  [
-    { year: 2014, price: 670, bedrooms: 0 },
-    { year: 2015, price: 720, bedrooms: 0 },
-    { year: 2016, price: 780, bedrooms: 0 },
-    { year: 2017, price: 830, bedrooms: 0 },
-    { year: 2018, price: 880, bedrooms: 0 },
-    { year: 2019, price: 880, bedrooms: 0 },
-    { year: 2020, price: 920, bedrooms: 0 },
-    { year: 2021, price: 1030, bedrooms: 0 },
-    { year: 2022, price: 1130, bedrooms: 0 },
-    { year: 2023, price: 1180, bedrooms: 0 }
+    {
+      year: 2014,
+      price: 740,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2015,
+      price: 760,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2016,
+      price: 760,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2017,
+      price: 870,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2018,
+      price: 910,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2019,
+      price: 980,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2020,
+      price: 1060,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2021,
+      price: 1060,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2022,
+      price: 1090,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    },
+    {
+      year: 2023,
+      price: 1190,
+      bedrooms: 0,
+      bathrooms: 1,
+      square_footage: 353
+    }
   ]
 ]
 
 const generatePrices = (properties) => {
 
   const queryArray = [];
+  let propertyId = 1;
 
-  for (let i = 0; i < properties.length; i++) {
-    queryArray.push(`(${i}, 1, 'approved', '2023-02-02', ${properties[i].price}, 'https://www2.gov.bc.ca/assets/gov/housing-and-tenancy/residential-tenancies/forms/rtb1_chrome.pdf', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fvancouverforliving.com%2Ffurnished%2Fvancouver-apartments%2F&psig=AOvVaw1ysF0BbEEP4fkSz4PyvgIm&ust=1675718793700000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjqxY2p__wCFQAAAAAdAAAAABAE', 'apartment', 1000, 2, 1)`);
+  // console.log(properties)
+
+  for (const property of properties) {
+    for (let i = 0; i < property.length; i++) {
+      queryArray.push(`(${propertyId}, 1, 'approved', '${property[i].year}-02-02', ${property[i].price}, 'https://www2.gov.bc.ca/assets/gov/housing-and-tenancy/residential-tenancies/forms/rtb1_chrome.pdf', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fvancouverforliving.com%2Ffurnished%2Fvancouver-apartments%2F&psig=AOvVaw1ysF0BbEEP4fkSz4PyvgIm&ust=1675718793700000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjqxY2p__wCFQAAAAAdAAAAABAE', 'apartment', ${property[i].square_footage}, ${property[i].bedrooms}, ${property[i].bathrooms}),`);
+    }
+    propertyId = propertyId + 1;
   }
 
-  console.log(queryArray);
+  console.log('queryArray', queryArray);
 
 }
+
+// CREATE TABLE prices (
+//   id SERIAL PRIMARY KEY NOT NULL,
+//   property_id INTEGER REFERENCES properties(id),
+//   user_id INTEGER REFERENCES users(id),
+//   admin_status VARCHAR(255) NOT NULL DEFAULT "pending",
+//   date DATE NOT NULL,
+//   price NUMERIC NOT NULL,
+//   documentation VARCHAR(255) NOT NULL, --url
+//   photo VARCHAR(255), --url 
+//   property_type VARCHAR(255) NOT NULL,
+//   square_footage INTEGER NOT NULL,
+//   number_of_bedrooms INTEGER NOT NULL,
+//   number_of_bathrooms INTEGER NOT NULL,  
+// );
 
 generatePrices(studios)
