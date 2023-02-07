ca50c3f17f8060ace1ad494b7f3751a73b9e50bb
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-06 18:55:34 -0800

    made arrays of price data for each size of apartment

---
+++ server/helpers/generatePriceSeeds.js
@@ -0,0 +1,169 @@
+const roundToNearest10 = (num) => {
+  return Math.round(num / 10) * 10;
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// A function to increase prices, used to create seed data over years. 
+// It outputs an array of arrays, with each index containing a year, and the rent price for that year/
+// Now need to format this in an SQL CREATE function. 
+
+const generateStudioPrices = () => {
+
+  let bedrooms = 0
+  let query = [];
+
+  for (let i = 0; i < 50; i++) {
+    let price = getRandomInt(600, 1000)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 7))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+// console.log(generateStudioPrices())
+
+
+const generateOneBedroomPrices = () => {
+  let bedrooms = 1
+  let query = [];
+
+  for (let i = 0; i < 50; i++) {
+    let price = getRandomInt(900, 1400)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+// console.log(generateOneBedroomPrices())
+
+const generateTwoBedroomPrices = () => {
+  let bedrooms = 2
+  let query = [];
+
+  for (let i = 0; i < 50; i++) {
+    let price = getRandomInt(800, 1700)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+// console.log(generateTwoBedroomPrices())
+
+
+const generateThreeBedroomPrices = () => {
+  let bedrooms = 3
+  let query = [];
+
+  for (let i = 0; i < 50; i++) {
+    let price = getRandomInt(1000, 2000)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+// console.log(generateThreeBedroomPrices())
+
+const generateFourBedroomPrices = () => {
+  let bedrooms = 4
+  let query = [];
+
+  for (let i = 0; i < 15; i++) {
+    let price = getRandomInt(1500, 3000)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+
+// console.log(generateFourBedroomPrices())
+
+const generateFiveBedroomPrices = () => {
+  let bedrooms = 5
+  let query = [];
+
+  for (let i = 0; i < 15; i++) {
+    let price = getRandomInt(1700, 3500)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+// console.log(generateFiveBedroomPrices())
+
+const generateSixBedroomPrices = () => {
+  let bedrooms = 6
+  let query = [];
+
+  for (let i = 0; i < 7; i++) {
+    let price = getRandomInt(2500, 4300)
+    let propertyPriceHistory = [];
+    for (let year = 2014; year < 2024; year++) {
+      price = roundToNearest10(price + Math.round(price * Math.random() / 6))
+      propertyPriceHistory.push({'year': year, 'price': price, 'bedrooms': bedrooms})
+    }
+    query.push(propertyPriceHistory)
+  }
+  
+  return query
+}
+
+console.log(generateSixBedroomPrices())
+
+
+
+
+// const generateProperties = (prices) => {
+
+//   const queryArray = [];
+
+//   for (let i = 0; i < addresses.length; i++) {
+//     queryArray.push(`('BC', 'Vancouver', '${addresses[i].address}', ${addresses[i].lat}, ${addresses[i].lng}),`);
+//   }
+
+//   // for (let address of addresses) {
+//   //   queryArray.push(`('BC', 'Vancouver', '${address.address}', ${address.lat}, ${address.lng}),`);
+//   // }
+
+//   console.log(queryArray);
+
+// }
--- server/helpers/generateSeeds.js
+++ server/helpers/generateSeeds.js
@@ -6,12 +6,12 @@ const generateProperties = (addresses) => {
 
   const queryArray = [];
 
-  for (let i = 200; i < addresses.length; i++) {
+  for (let i = 0; i < addresses.length; i++) {
     queryArray.push(`('BC', 'Vancouver', '${addresses[i].address}', ${addresses[i].lat}, ${addresses[i].lng}),`);
   }
 
   // for (let address of addresses) {
-  //   queryArray.push(`('BC', 'Vancouver', ${address.address}, ${address.lat}, ${address.lng}),`);
+  //   queryArray.push(`('BC', 'Vancouver', '${address.address}', ${address.lat}, ${address.lng}),`);
   // }
 
   console.log(queryArray);
@@ -19,8 +19,5 @@ const generateProperties = (addresses) => {
 }
 
 generateProperties(addresses)
-
-
-// INSERT INTO properties (province, city, street_address, latitude, longitude) VALUES 
 
 
