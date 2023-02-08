05bcd7f50a0caf08bd6612e49f1869a2b83fa9e8
Michael Green <greenmr95@hotmail.com>
2023-02-08 02:33:01 -0800

    protect propertyid and priceid routes for admin use only and add edit button

--- client/src/components/admin/PriceDetails.jsx
+++ client/src/components/admin/PriceDetails.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ReactSession } from 'react-client-session';
 import { useHistory, useParams } from "react-router-dom";
 
 const PriceDetails = () => {
 
+  const history = useHistory()
+  const userRole = ReactSession.get("role");
+  if (userRole !== 'admin') {
+    history.push('/')
+    window.location.reload();
+  }
+
   const id = useParams().priceid
   const [price, setPrice] = useState(null)
   const [isPending, setIsPending] = useState(false);
-  const history = useHistory()
 
   useEffect(() => {
   axios.get(`http://localhost:8000/prices/${id}`)
@@ -40,8 +47,9 @@ const PriceDetails = () => {
           <h3>{price.price}</h3>
           <h3>{price.property_type}, {price.square_footage}</h3>
           <h3>{price.number_of_bedrooms},{price.number_of_bathrooms}</h3>
+          <button>Edit</button>
           { !isPending && <button onClick={handleClick}>Delete</button> }
-          { isPending && <button disabled>Deleting Price...</button> }
+          {isPending && <button disabled>Deleting Price...</button>}
         </div>  
       )}
     </li>    
--- client/src/components/admin/PropertyDetails.jsx
+++ client/src/components/admin/PropertyDetails.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ReactSession } from 'react-client-session';
 import { useHistory, useParams } from "react-router-dom";
 
 const PropertyDetails = () => {
 
+  const history = useHistory()
+  const userRole = ReactSession.get("role");
+  if (userRole !== 'admin') {
+    history.push('/')
+    window.location.reload();
+  }
+
   const id = useParams().propertyid
   const [property, setProperty] = useState(null)
   const [isPending, setIsPending] = useState(false);
-  const history = useHistory()
 
   useEffect(() => {
   axios.get(`http://localhost:8000/properties/${id}`)
@@ -38,7 +45,8 @@ const PropertyDetails = () => {
         <div>
           <h3>{property.street_address}</h3>          
           <h3>{property.province}, {property.city}</h3>
-          <h3>{property.latitude},{property.longitude }</h3>
+          <h3>{property.latitude},{property.longitude}</h3>
+          <button>Edit</button>
           { !isPending && <button onClick={handleClick}>Delete</button> }
           { isPending && <button disabled>Deleting Property...</button> }
         </div>  
