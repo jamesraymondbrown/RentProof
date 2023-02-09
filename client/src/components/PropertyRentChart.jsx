7ae71f75887b2a07530c0820ab2feaab28cd366f
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-09 12:24:34 -0800

    add scss file for the rent chart

--- client/src/components/PropertyRentChart.jsx
+++ client/src/components/PropertyRentChart.jsx
@@ -23,34 +23,40 @@ const PropertyRentChart = (props) => {
   }
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <AreaChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Area dataKey="price" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="date" />
-        <YAxis
-          dataKey="price"
-          domain={[
-            parseInt(data[0].price) - 500,
-            parseInt(data[data.length - 1].price) + 500,
-          ]}
-        />
-        <Tooltip />
-      </AreaChart>
-      {/* <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="price" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="date" />
-        <YAxis
-          dataKey="price"
-          domain={[
-            parseInt(data[0].price) - 500,
-            parseInt(data[data.length - 1].price) + 500,
-          ]}
-        />
-        <Tooltip />
-      </LineChart> */}
-    </ResponsiveContainer>
+    <div>
+      <div className="chart-title">Price History:</div>
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Area dataKey="price" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="date" />
+          <YAxis
+            dataKey="price"
+            domain={[
+              parseInt(data[0].price) - 500,
+              parseInt(data[data.length - 1].price) + 500,
+            ]}
+          />
+          <Tooltip />
+        </AreaChart>
+        {/* <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="price" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="date" />
+          <YAxis
+            dataKey="price"
+            domain={[
+              parseInt(data[0].price) - 500,
+              parseInt(data[data.length - 1].price) + 500,
+            ]}
+          />
+          <Tooltip />
+        </LineChart> */}
+      </ResponsiveContainer>
+    </div>
   );
 };
 
