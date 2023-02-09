0dd3f2f85a2e9fc800e206240fefd1dadfd5cf1e
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-09 09:23:51 -0800

    fixed the seeds so that product images show

--- client/src/components/helpers/getDataFromPrices.js
+++ client/src/components/helpers/getDataFromPrices.js
@@ -10,10 +10,19 @@ const getPhotoFromPrices = (property, prices) => {
   return photo;
 };
 
+const findLastIndex = (array, predicate) => {
+  var l = array.length;
+  while (l--) {
+    if (predicate(array[l], l, array)) return l;
+  }
+  return -1;
+};
+
 const getCostFromPrices = (property, prices) => {
   let cost = 2000;
-  for (let price of prices) {
-    // console.log("price");
+  const reversedPrices = [...prices].reverse();
+  // console.log("reverse", reversedPrices);
+  for (let price of reversedPrices) {
     if (price.property_id === property.id) {
       cost = price.price;
       return cost;
@@ -21,6 +30,9 @@ const getCostFromPrices = (property, prices) => {
   }
   return cost;
 };
+
+// const lastIndex = prices.map(price =>
+//   price.property_id === currentShape).lastIndexOf(true);
 
 const getBedroomsFromPrices = (property, prices) => {
   let bedrooms = 2;
