6e99966295374158eb066d4ed83478ffe7162c77
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-09 14:37:25 -0800

    change chart colors and tooltip

--- client/src/components/PropertyRentChart.jsx
+++ client/src/components/PropertyRentChart.jsx
@@ -9,9 +9,23 @@ import {
   YAxis,
   Tooltip,
 } from "recharts";
+import "./PropertyRentChart.scss";
 
 const PropertyRentChart = (props) => {
   const data = [];
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label tooltip-text">{`${label}`}</p>
+          <p className="tooltip-text">{`price: $${payload[0].value}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   for (let price of props.prices) {
     if (price.admin_status === "approved") {
@@ -30,17 +44,32 @@ const PropertyRentChart = (props) => {
           data={data}
           margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
         >
-          <Area dataKey="price" />
+          <defs>
+            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#008631" stopOpacity={0.65} />
+              <stop offset="75%" stopColor="#008631" stopOpacity={0.15} />
+            </linearGradient>
+          </defs>
+
+          <Area dataKey="price" stroke="#008631" fill="url(#color)" />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="date" />
+          <XAxis
+            dataKey="date"
+            ticks={["2014", "2016", "2018", "2020", "2022", "2024"]}
+          />
           <YAxis
             dataKey="price"
             domain={[
               parseInt(data[0].price) - 500,
-              parseInt(data[data.length - 1].price) + 500,
+              parseInt(data[data.length - 1].price) + 200,
             ]}
+            tickCount={6}
+            tickFormatter={(price) => `$${price}`}
           />
-          <Tooltip />
+          <Tooltip
+            content={<CustomTooltip />}
+            wrapperStyle={{ outline: "none" }}
+          />
         </AreaChart>
         {/* <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
           <Line type="monotone" dataKey="price" stroke="#8884d8" />
