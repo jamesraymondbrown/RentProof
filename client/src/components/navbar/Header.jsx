a3a62597d548ffbde9661883cc4f9ee4f5812415
Michael Green <greenmr95@hotmail.com>
2023-02-11 19:08:59 -0800

    add welcome dialog to navbar

--- client/src/components/navbar/Header.jsx
+++ client/src/components/navbar/Header.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import Navbar from './Navbar';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+  const [visible, setVisible] = useState(false);
+
   return (
     <header>
       <div className="nav-area">
         <Link to="/" className="logo">
           Rent Tracker
         </Link>
+        <Button label="Welcome!" icon="pi pi-external-link" onClick={() => setVisible(true)} />
+        <Dialog header="What We Do ..." visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+          <p className="m-0">
+            Rent Tracker is a people-powered app that puts information into the hands of renters and provides public accountability
+            against exorbitant rent increases.
+            
+            <br /><br />Here, you can find statistical averages for your area and property-specific data when you click
+            on a property on the map.
+          </p>
+        </Dialog>
         <Navbar />
       </div>
     </header>
