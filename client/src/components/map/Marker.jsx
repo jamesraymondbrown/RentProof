de8ff7eec57cff569442223a4e18a178475f88aa
Hadley <sutherlandhadley@gmail.com>
2023-02-13 01:27:04 -0700

    adjust coloring and also add a dot on the symbol

--- client/src/components/map/Marker.jsx
+++ client/src/components/map/Marker.jsx
@@ -39,18 +39,72 @@ function percentageToHexColor(percentage, lightness = 0, saturation = 100) {
   return "#" + R + G + B;
 }
 
+function desaturate(hex, amount) {
+  // Remove the '#' from the beginning of the hex string
+  hex = hex.substring(1);
+
+  // Convert hex string to RGB values
+  let r = parseInt(hex.substring(0, 2), 16);
+  let g = parseInt(hex.substring(2, 4), 16);
+  let b = parseInt(hex.substring(4, 6), 16);
+
+  // Desaturate the color by averaging the RGB values and scaling them down
+  let avg = (r + g + b) / 3;
+  r = Math.round(avg + (r - avg) * (1 - amount));
+  g = Math.round(avg + (g - avg) * (1 - amount));
+  b = Math.round(avg + (b - avg) * (1 - amount));
+
+  // Convert the RGB values back to a hex string
+  let result = ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  return "#" + result;
+}
+
+function darken(hex, amount) {
+  // Remove the '#' from the beginning of the hex string
+  hex = hex.substring(1);
+
+  // Convert hex string to RGB values
+  let r = parseInt(hex.substring(0, 2), 16);
+  let g = parseInt(hex.substring(2, 4), 16);
+  let b = parseInt(hex.substring(4, 6), 16);
+
+  // Darken the color by decreasing the RGB values
+  r = Math.round(r * (1 - amount));
+  g = Math.round(g * (1 - amount));
+  b = Math.round(b * (1 - amount));
+
+  // Convert the RGB values back to a hex string
+  let result = ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  return "#" + result;
+}
+
+
 // console.log("state.prevProperty ➤", state.prevProperty);
   const isGray = state.prevProperty.includes(id);
   const costVSavg = cost / 25;
   const saturation = state.prevProperty.includes(id) ? 0 : 100;
   const markerColor = percentageToHexColor(costVSavg, 120);
+  const markerColorgreyed = desaturate(markerColor, 0.5);
+  const markerColorDark = darken(markerColor, 0.7);
 
-function pinSymbol(color, isGray, isHovered) {
+
+const pinSymbol2 = function (color, greycolor, isGray, isHovered) {
+  return {
+    path: "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z",
+    fillColor: isGray ? greycolor : color,
+    fillOpacity: 1,
+    strokeColor: isGray ? "#808080" : "#000",
+    strokeWeight: isHovered ? 3 : 2,
+    scale: 0.5,
+    // anchor: [0.5, 0.5] doesnt work
+  };
+};
+function pinSymbol1(color, greycolor, darkColor, isGray, isHovered) {
   return {
     path: "M23.667 11.84c0 7.52-10.048 19.52-11.5 19.52s-11.5-12-11.5-19.52C.667 5.743 5.816.8 12.167.8c6.35 0 11.5 4.943 11.5 11.04",
-    fillColor: color,
-    fillOpacity: isGray ? 0.7 : 1,
-    strokeColor: isGray ? "#808080" : "#000",
+    fillColor: isGray ? greycolor : color,
+    fillOpacity: 1,
+    strokeColor: isGray ? "#808080" : darkColor,
     strokeWeight: isHovered ? 3 : 2,
     scale: 1,
   };
@@ -64,11 +118,19 @@ function pinSymbol(color, isGray, isHovered) {
      cost < maxF &&
      cost > minF ? (
        <Suspense fallback={<div>Loading...</div>}>
+         {/* Marker 1 */}
          <GoogleMapMarker
            position={position}
            title={title}
+           zIndex={isGray ? 0 : 1}
            onClick={() => handleClickMarker(id)}
-           icon={pinSymbol(markerColor, isGray, isHovered)}
+           icon={pinSymbol1(
+             markerColor,
+             markerColorgreyed,
+             markerColorDark,
+             isGray,
+             isHovered
+           )}
            onMouseOver={() => {
              setIsHovered(true);
            }}
@@ -85,10 +147,39 @@ function pinSymbol(color, isGray, isHovered) {
              return 2;
            })()}
          />
+         {/* Marker 2
+         <GoogleMapMarker
+           position={position}
+           title={title}
+           zIndex={isGray ? 2 : 3}
+           onClick={() => handleClickMarker(id)}
+           icon={pinSymbol2(
+             markerColor,
+             markerColorgreyed,
+             isGray,
+             isHovered
+           )}
+           onMouseOver={() => {
+             setIsHovered(true);
+           }}
+           onMouseOut={() => {
+             setIsHovered(false);
+           }}
+           animation={(() => {
+             if (state.currentProperty.id === id) {
+               return 1;
+             }
+             if (state.prevProperty.includes(id)) {
+               return null;
+             }
+             return 2;
+           })()}
+         /> */}
        </Suspense>
      ) : null}
    </div>
  );
+
 
 }
 
